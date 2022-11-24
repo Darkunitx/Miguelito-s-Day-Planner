f7e4af40625eb9c0472b1e5ef0e8fc9f8f014e5a
Darkunitx <darkunitx@live.com>
2022-11-24 09:44:48 -0600

    added value to the containers by their Ids

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-
+ 
 var timeDisplay = document.querySelector("#currentDay");
 
 function displayTime(today) {
@@ -11,6 +11,52 @@ function displayTime(today) {
 }
 
 displayTime();
+
+function backgroundColorSetter () {
+
+  function getCurrentHour(currentHour) {
+    var currentHour = dayjs();
+    console.log(currentHour.format("H"));
+  }
+
+  getCurrentHour();
+
+
+  var nine = document.getElementById('hour-9').value=9;
+  var ten = document.getElementById('hour-10').value=10;
+  var eleven = document.getElementById('hour-11').value=11;
+  var twelve = document.getElementById('hour-12').value=12;
+  var thirteen = document.getElementById('hour-13').value=13;
+  var fourteen = document.getElementById('hour-14').value=14;
+  var fifteen = document.getElementById('hour-15').value=15;
+  var sixteen = document.getElementById('hour-16').value=16;
+  var seventeen = document.getElementById('hour-17').value=17;
+
+  // console.log(document.getElementById('hour-9').value);
+  // console.log(document.getElementById('hour-10').value);
+  // console.log(document.getElementById('hour-11').value);
+  // console.log(document.getElementById('hour-12').value);
+  // console.log(document.getElementById('hour-13').value);
+  // console.log(document.getElementById('hour-14').value);
+  // console.log(document.getElementById('hour-15').value);
+  // console.log(document.getElementById('hour-16').value);
+  // console.log(document.getElementById('hour-17').value);
+
+  
+
+  
+
+    
+  }
+
+
+
+
+  // .attr('class', 'past');
+  // .attr('class', 'present');
+  // .attr('class', 'future');
+
+backgroundColorSetter();
 
 
 function plannerFunction() {
@@ -24,6 +70,8 @@ function plannerFunction() {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
+
+
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
